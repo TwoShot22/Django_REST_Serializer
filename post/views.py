2f9b531304b5c1f9e23eed45f6db9a5c2c5c9851
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-19 22:56:56 +0900

    Add viewsets in View / router in urls

--- post/views.py
+++ post/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post
+from .serializer import PostSerializer
 
-# Create your views here.
+# CBV
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
